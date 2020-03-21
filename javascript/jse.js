79a091fa944cf65804b285424874a1789d2da86c
Joao Vitor Viegas Rego <jvrviegas@gmail.com>
2020-03-21 20:53:42 -0300

    D - Função Capturar - Adicionado preventDefault ao botão

--- javascript/jse.js
+++ javascript/jse.js
@@ -1,9 +1,12 @@
 function capturar(){
+    document.getElementById("botaoEnviar").addEventListener("click", function(e) {
+        e.preventDefault();
+    });
     var id = document.getElementById("inputId").value;
     if(id==""){
         alert('Preencha o campo email.');
     }else {
-    alert('O email ' +id+' foi enviado!');
-    document.getElementById('inputId').value='';
- }
+        alert('O email ' +id+' foi enviado!');
+        document.getElementById('inputId').value='';
+    }
 }
